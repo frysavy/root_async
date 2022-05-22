eba978719d063076d1ef7a8e79d4252c958344b9
Filip Ryšavý <filip.rysavy@aponi.sk>
2022-05-22 21:51:08 +0200

    Change for publishing rules

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,8 +16,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  int ahoj = 0;
+  String _output = 'Unknown';
 
   @override
   void initState() {
@@ -25,25 +24,18 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
+    String output;
     try {
-      platformVersion =
-          await RootAsync.exec("su") ?? "Unknown platform version";
+      output = await RootAsync.exec("ls") ?? "LS Output";
     } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+      output = 'Failed to get LS.';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {
-      _platformVersion = platformVersion;
+      _output = output;
     });
   }
 
@@ -52,11 +44,11 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Root_async'),
         ),
         body: Center(
           child: Column(children: [
-            Text('Running on: $_platformVersion\n'),
+            Text('Ls output: $_output\n'),
           ]),
         ),
         floatingActionButton:
